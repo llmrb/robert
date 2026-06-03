59709b2532f9bc788917cb7c4fce48134da4196f
0x1eef <0x1eef@hardenedbsd.org>
2026-06-03 09:17:02 -0300

    build: mruby-llm v0.1.0.beta.8

--- build.rb
+++ build.rb
@@ -25,7 +25,7 @@ MRuby::Build.new("robert") do |conf|
   conf.gem git: "https://github.com/mrbgemz/mruby-tui"      , branch: "v0.4.0"
   conf.gem git: "https://github.com/mrbgemz/mruby-tui-chat" , branch: "v0.3.0"
   conf.gem git: "https://github.com/mrbgemz/mruby-markdown" , branch: "main"
-  conf.gem git: "https://github.com/llmrb/mruby-llm"        , branch: "v0.1.0.beta.7"
+  conf.gem git: "https://github.com/llmrb/mruby-llm"        , branch: "v0.1.0.beta.8"
   conf.gem git: "https://github.com/0x1eef/mruby-command"   , branch: "main"
   conf.gem File.expand_path(__dir__)
 
